3db9fbfa807bc2b6e1b7150662e2fb5082374bfc
twallace27603 <tswwci@hotmail.com>
2021-03-17 09:26:16 -0400

    Added Settings Display

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CloudFunBC.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace CloudFunBC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
         {
-           
+            ViewBag.ShowSetting = false;
+            if (!string.IsNullOrEmpty(_config.GetValue<string>("RegularSetting")))
+            {
+                ViewBag.Regular = _config.GetValue<string>("RegularSetting");
+                ViewBag.ShowSetting = true;
+            }
+            if (!string.IsNullOrEmpty(_config.GetValue<string>("SensitiveSetting")))
+            {
+                ViewBag.Sensitive = _config.GetValue<string>("SensitiveSetting");
+                ViewBag.ShowSetting = true;
+            }
+            
+
             return View();
         }
 
